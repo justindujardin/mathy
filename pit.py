8695fe078f7308ebfd37813807d3bfb376211156
Justin DuJardin <justin@dujardinconsulting.com>
2018-08-25 16:01:45 -0700

    Update pit to use epsilon = 0 for arena
     - this removes dirichlet noise when in the arena.

--- pit.py
+++ pit.py
@@ -16,12 +16,12 @@ rp = RandomPlayer(g).play
 
 n1 = MathNeuralNet(g)
 n1.load_checkpoint("./training/latest/latest.pth.tar")
-mcts1 = MCTS(g, n1, cpuct=1.0, num_mcts_sims=100)
+mcts1 = MCTS(g, n1, cpuct=1.0, num_mcts_sims=100, epsilon=0)
 calvin = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
 # n2 = MathNeuralNet(g)
 # n2.load_checkpoint("./training/08_11_18_1hr/latest.pth.tar")
-# mcts2 = MCTS(g, n2, cpuct=1.0, num_mcts_sims=100)
+# mcts2 = MCTS(g, n2, cpuct=1.0, num_mcts_sims=100, epsilon=0)
 # algernon = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 algernon = rp
 
