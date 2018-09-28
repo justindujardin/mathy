4a300449bff5899cfbfafa56118343e8b3ab7a19
Justin DuJardin <justin@dujardinconsulting.com>
2018-09-28 15:23:43 -0700

    drop the unused requirements

--- mathzero/model/torch_model.py
+++ mathzero/model/torch_model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
 
 
 class MathModel(nn.Module):
@@ -14,7 +13,9 @@ class MathModel(nn.Module):
 
         super(MathModel, self).__init__()
         self.conv1 = nn.Conv2d(1, args.num_channels, 3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(
+            args.num_channels, args.num_channels, 3, stride=1, padding=1
+        )
         self.conv3 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1)
         # self.conv4 = nn.Conv2d(args.num_channels, args.num_channels, 3, stride=1)
 
@@ -23,7 +24,9 @@ class MathModel(nn.Module):
         self.bn3 = nn.BatchNorm2d(args.num_channels)
         self.bn4 = nn.BatchNorm2d(args.num_channels)
 
-        self.fc1 = nn.Linear(args.num_channels*(self.board_x-2)*(self.board_y-2), 64)
+        self.fc1 = nn.Linear(
+            args.num_channels * (self.board_x - 2) * (self.board_y - 2), 64
+        )
         self.fc_bn1 = nn.BatchNorm1d(64)
 
         self.fc2 = nn.Linear(64, 32)
@@ -35,17 +38,33 @@ class MathModel(nn.Module):
 
     def forward(self, s):
         #                                                           s: batch_size x board_x x board_y
-        s = s.view(-1, 1, self.board_x, self.board_y)                # batch_size x 1 x board_x x board_y
-        s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y
-        s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
-        s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2)
+        s = s.view(
+            -1, 1, self.board_x, self.board_y
+        )  # batch_size x 1 x board_x x board_y
+        s = F.relu(
+            self.bn1(self.conv1(s))
+        )  # batch_size x num_channels x board_x x board_y
+        s = F.relu(
+            self.bn2(self.conv2(s))
+        )  # batch_size x num_channels x board_x x board_y
+        s = F.relu(
+            self.bn3(self.conv3(s))
+        )  # batch_size x num_channels x (board_x-2) x (board_y-2)
         # s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4)
-        s = s.view(-1, self.args.num_channels*(self.board_x-2)*(self.board_y-2))
+        s = s.view(-1, self.args.num_channels * (self.board_x - 2) * (self.board_y - 2))
 
-        s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)  # batch_size x 64
-        s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.args.dropout, training=self.training)  # batch_size x 32
+        s = F.dropout(
+            F.relu(self.fc_bn1(self.fc1(s))),
+            p=self.args.dropout,
+            training=self.training,
+        )  # batch_size x 64
+        s = F.dropout(
+            F.relu(self.fc_bn2(self.fc2(s))),
+            p=self.args.dropout,
+            training=self.training,
+        )  # batch_size x 32
 
-        pi = self.fc3(s)                                                                         # batch_size x action_size
-        v = self.fc4(s)                                                                          # batch_size x 1
+        pi = self.fc3(s)  # batch_size x action_size
+        v = self.fc4(s)  # batch_size x 1
 
         return F.log_softmax(pi, dim=1), F.tanh(v)
