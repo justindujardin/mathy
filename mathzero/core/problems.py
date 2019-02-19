524db80d655f6f45b4b0abddd154eff79ef7d03a
Justin DuJardin <justin@dujardinconsulting.com>
2019-02-18 20:13:02 -0800

    fix(problems): min/max complexity were swapped

--- mathzero/core/problems.py
+++ mathzero/core/problems.py
@@ -9,8 +9,8 @@ MODE_SIMPLIFY_POLYNOMIAL = 2
 class ProblemGenerator:
     def __init__(self):
         self.max_int = 2048
-        self.max_complexity = 3
-        self.min_complexity = 7
+        self.max_complexity = 7
+        self.min_complexity = 3
         self.variables = list("xyz")
         self.operators = list("+*")
         self.problem_types = [
