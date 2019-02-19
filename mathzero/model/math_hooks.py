472f3083a7f247d18c7da9cd6315558f91dc45cd
Justin DuJardin <justin@dujardinconsulting.com>
2019-02-19 10:27:56 -0800

    chore: cleanup training logger

--- mathzero/model/math_hooks.py
+++ mathzero/model/math_hooks.py
@@ -5,14 +5,10 @@ import sys
 from datetime import datetime
 
 
-# This is defined inside the class because we don't import tf at
-# the module level. Multi-process self-play explodes if tf is
-# imported at the top of the file.
 class TrainingLoggerHook(tf.train.SessionRunHook):
-    """Logs loss and runtime."""
+    """Log training progress to the console, including pi and value losses"""
 
     def __init__(self, log_frequency, batch_size):
-        self.log_frequency = log_frequency
         self.batch_size = batch_size
 
     def begin(self):
@@ -28,7 +24,7 @@ class TrainingLoggerHook(tf.train.SessionRunHook):
         duration = current_time - self._start_time
         self._start_time = current_time
         loss_pi, loss_v = run_values.results
-        examples_per_sec = self.log_frequency * self.batch_size / duration
+        examples_per_sec = self.batch_size / duration
         sec_per_batch = duration
         template = "%s: step %d, loss_pi = %.2f, loss_v = %.2f (%.1f examples/sec; %.3f sec/batch)"
         args = (
@@ -40,4 +36,4 @@ class TrainingLoggerHook(tf.train.SessionRunHook):
             sec_per_batch,
         )
         print(template % args)
-        sys.stdout.flush()        
+        sys.stdout.flush()
