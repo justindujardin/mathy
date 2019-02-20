57362b8d29a78a15fbf170d61ca840a96960b555
Justin DuJardin <justin@dujardinconsulting.com>
2019-02-19 19:47:46 -0800

    only log every n steps and include overall loss

--- mathzero/model/math_hooks.py
+++ mathzero/model/math_hooks.py
@@ -8,8 +8,9 @@ from datetime import datetime
 class TrainingLoggerHook(tf.train.SessionRunHook):
     """Log training progress to the console, including pi and value losses"""
 
-    def __init__(self, batch_size):
+    def __init__(self, batch_size, log_every_n=10):
         self.batch_size = batch_size
+        self.log_every_n = log_every_n
 
     def begin(self):
         self._step = -1
@@ -22,14 +23,17 @@ class TrainingLoggerHook(tf.train.SessionRunHook):
     def after_run(self, run_context, run_values):
         current_time = time.time()
         duration = current_time - self._start_time
+        if self._step % self.log_every_n != 0:
+            return
         self._start_time = current_time
         loss_pi, loss_v = run_values.results
         examples_per_sec = self.batch_size / duration
         sec_per_batch = duration
-        template = "%s: step %d, loss_pi = %.2f, loss_v = %.2f (%.1f examples/sec; %.3f sec/batch)"
+        template = "%s: step %d, loss = %.3f loss_pi = %.3f, loss_v = %.3f (%.1f examples/sec; %.3f sec/batch)"
         args = (
             datetime.now(),
             self._step,
+            loss_pi + loss_v,
             loss_pi,
             loss_v,
             examples_per_sec,
