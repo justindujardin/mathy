47e120722fc27946942079b6b09d2b36c426ece9
Justin DuJardin <justin@dujardinconsulting.com>
2019-04-01 16:37:40 -0700

    chore: missed training features

--- mathzero/model/math_dataset.py
+++ mathzero/model/math_dataset.py
@@ -26,9 +26,9 @@ def make_training_input_fn(examples, batch_size):
     output_types = (
         {
             FEATURE_FWD_VECTORS: tf.string,
-            # FEATURE_BWD_VECTORS: tf.int8,
-            # FEATURE_LAST_FWD_VECTORS: tf.string,
-            # FEATURE_LAST_BWD_VECTORS: tf.int8,
+            FEATURE_BWD_VECTORS: tf.string,
+            FEATURE_LAST_FWD_VECTORS: tf.string,
+            FEATURE_LAST_BWD_VECTORS: tf.string,
             FEATURE_NODE_COUNT: tf.int32,
             FEATURE_MOVE_COUNTER: tf.int32,
             FEATURE_MOVES_REMAINING: tf.int32,
