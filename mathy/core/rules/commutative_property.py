af9da78f10c78e1a4a5716d5513891a01242fcca
Justin DuJardin <justin@dujardinconsulting.com>
2019-05-03 15:50:18 -0700

    feat(commutative_property): exclude natural order complex terms
     - when `preferred` is false for the commutative rule, exclude terms with exponents that are in natural order already (i.e. 8y^3 not y^3 * 8

--- mathy/core/rules/commutative_property.py
+++ mathy/core/rules/commutative_property.py
@@ -5,12 +5,7 @@ from ..expressions import (
     VariableExpression,
     PowerExpression,
 )
-from ..util import (
-    is_add_or_sub,
-    is_const,
-    terms_are_like,
-    is_preferred_term_form,
-)
+from ..util import is_add_or_sub, is_const, terms_are_like, is_preferred_term_form
 from ..rule import BaseRule
 
 # ### Commutative Property
@@ -51,12 +46,20 @@ class CommutativeSwapRule(BaseRule):
             return True
         if not isinstance(node, MultiplyExpression):
             return False
+        # When preferred is false, the commutative rule won't apply to term
+        # nodes that are already in a preferred position.
         if self.preferred is False:
-            # 4x won't commute to x * 4 if preferred is false because 4x is the preferred term order
+            # 4x won't commute to x * 4
             left_const = isinstance(node.left, ConstantExpression)
             right_var = isinstance(node.right, VariableExpression)
             if left_const and right_var:
                 return False
+            # 8y^4 won't commute to y^4 * 8
+            if isinstance(node.right, PowerExpression):
+                right_left_var = isinstance(node.right.left, VariableExpression)
+                right_right_const = isinstance(node.right.right, ConstantExpression)
+                if right_left_var and right_right_const:
+                    return False
         return True
 
     def apply_to(self, node):
