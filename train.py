fix(polynomials): remove difficulty variance
 - so the polynomial generator would pick from a list of variables at random to build problems, but this would result in some problems that were much simpler than others, making it difficult to know if the policy was getting better or if the problems just happened to be the easy variants. For example for a 5 term polynomial you could get "2x + 3x + 4x + 5y + x" where the X terms are already side-by-side and ready to be factored and combined.
 - now polynomial generators will generate a list of like terms and repeat it in order so that no terms start immediately next to a like term. The example above would now always look like: "2x + 3y + x + 4y + z"

train.py
@@ -39,12 +39,19 @@ tf.compat.v1.logging.set_verbosity("CRITICAL")
         str,
     ),
     learning_rate=("The learning rate to use when training", "option", "lr", float),
+    epochs=("The number of training epochs", "option", "e", int),
     dropout=("The dropout to apply to output predictions", "option", "d", float),
 )
-def main(model_dir, examples_file, transfer_from=None, learning_rate=3e-4, dropout=0.2):
-    epochs = 10
+def main(
+    model_dir,
+    examples_file,
+    transfer_from=None,
+    epochs=10,
+    learning_rate=3e-4,
+    dropout=0.2,
+):
     train_all = True
-    train_number = 2048
+    train_number = 2048 if not train_all else 1e6
     controller = MathGame(verbose=True)
     input_examples = Path(examples_file)
     model_dir = Path(model_dir)