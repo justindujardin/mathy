89e16afdf8f8c85478a0a104949aa52467da10cf
Justin DuJardin <justin@dujardinconsulting.com>
2019-06-01 09:48:49 -0700

    feat: add masking to LSTM

--- mathy/agent/layers/lstm.py
+++ mathy/agent/layers/lstm.py
@@ -10,18 +10,20 @@ def LSTM(units=32, name="lstm", use_shared=False):
         lstm = tf.keras.layers.LSTM(
             units, return_sequences=True, return_state=True, name="lstm"
         )
+        mask = tf.keras.layers.Masking()
         combine = tf.keras.layers.Add(name="add")
-        return lstm, combine
+        return lstm, combine, mask
 
     if use_shared:
         with tf.compat.v1.variable_scope(name, reuse=True, auxiliary_name_scope=False):
-            lstm, combine = build()
+            lstm, combine, mask = build()
     else:
         with tf.compat.v1.variable_scope(name, auxiliary_name_scope=False):
-            normalize, dense, activate, combine = build()
+            lstm, combine, mask = build()
 
     def func(input_layer, initial_state=None):
         with tf.compat.v1.variable_scope(name):
+            input_layer = mask(input_layer)
             # Support seeding the LSTM with initial state from some other input
             # Inspired by: https://cs.stanford.edu/people/karpathy/cvpr2015.pdf
             lstm_fwd, state_h_fwd, state_c_fwd = lstm(
