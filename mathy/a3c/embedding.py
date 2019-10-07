71240870ed9e97871bbb734803f33c3089f5b15c
Justin DuJardin <justin@dujardinconsulting.com>
2019-10-06 17:30:24 -0700

    :sparkles: add multi-head attention and parameterize embeddings more
     - add a config option to specify the extract_window width for context vectors. It defaults to None which represents each vector with a single integer type_id.
     - add config option for combining the problem type hashes to tokens before embedding them.
     - both of these options could use comparative runs to see which values make sense across multiple tasks.
     - remove the resnet and replace with multi-head attention over the input sequences

--- mathy/a3c/embedding.py
+++ mathy/a3c/embedding.py
@@ -1,7 +1,8 @@
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Optional, Tuple
 
 import tensorflow as tf
 
+from ..agent.layers.keras_self_attention.multi_head_attention import MultiHeadAttention
 from ..agent.layers.resnet_stack import ResNetStack
 from ..core.expressions import MathTypeKeysMax
 from ..features import (
@@ -16,25 +17,38 @@ from ..features import (
     FEATURE_PROBLEM_TYPE,
 )
 from ..state import (
+    PROBLEM_TYPE_HASH_BUCKETS,
     MathyBatchObservation,
     MathyObservation,
     MathyWindowObservation,
     observations_to_window,
-    PROBLEM_TYPE_HASH_BUCKETS,
 )
 
 
 class MathyEmbedding(tf.keras.layers.Layer):
     def __init__(
-        self, units: int = 128, lstm_units: int = 256, burn_in_steps: int = 10, **kwargs
+        self,
+        units: int = 128,
+        lstm_units: int = 256,
+        burn_in_steps: int = 10,
+        extract_window: Optional[int] = None,
+        encode_tokens_with_type: bool = False,
+        **kwargs,
     ):
         super(MathyEmbedding, self).__init__(**kwargs)
         self.units = units
+        self.encode_tokens_with_type = encode_tokens_with_type
+        self.extract_window = extract_window
         self.lstm_units = lstm_units
         self.burn_in_steps = burn_in_steps
         self.init_rnn_state()
+        token_in_dimension = (
+            PROBLEM_TYPE_HASH_BUCKETS
+            if self.encode_tokens_with_type
+            else MathTypeKeysMax
+        )
         self.token_embedding = tf.keras.layers.Embedding(
-            input_dim=PROBLEM_TYPE_HASH_BUCKETS,
+            input_dim=token_in_dimension,
             output_dim=self.lstm_units,
             name="nodes_embedding",
             mask_zero=True,
@@ -51,12 +65,9 @@ class MathyEmbedding(tf.keras.layers.Layer):
             name="hints_embedding",
             mask_zero=False,
         )
-        self.attention = tf.keras.layers.AdditiveAttention(name="attention")
+        self.attention = MultiHeadAttention(head_num=8, name="multi_head_attention")
         self.flatten = tf.keras.layers.Flatten(name="flatten")
         self.concat = tf.keras.layers.Concatenate(name=f"embedding_concat")
-        self.resnet = ResNetStack(
-            units=units // 12, name="embedding_resnet", num_layers=12
-        )
         self.time_lstm = tf.keras.layers.LSTM(
             self.lstm_units,
             name="timestep_lstm",
@@ -97,40 +108,42 @@ class MathyEmbedding(tf.keras.layers.Layer):
         batch_size = len(features.nodes)  # noqa
         sequence_length = len(features.nodes[0])
         type = tf.convert_to_tensor(features.type)
-        nodes = tf.convert_to_tensor(features.nodes)
+        input = tf.convert_to_tensor(features.nodes)
 
         #
         # Contextualize nodes by expanding their integers to include (n) neighbors
         #
-        extract_window = 6
-        # reshape to 4 dimensions so we can use `extract_patches`
-        input = tf.reshape(nodes, shape=[1, batch_size, sequence_length, 1])
-        input_one = tf.image.extract_patches(
-            images=input,
-            sizes=[1, 1, extract_window, 1],
-            strides=[1, 1, 1, 1],
-            rates=[1, 1, 1, 1],
-            padding="SAME",
-        )
-        # Remove the extra dimensions
-        input = tf.squeeze(input_one, axis=0)
+        if self.extract_window is not None:
+            # reshape to 4 dimensions so we can use `extract_patches`
+            input = tf.reshape(input, shape=[1, batch_size, sequence_length, 1])
+            input_one = tf.image.extract_patches(
+                images=input,
+                sizes=[1, 1, self.extract_window, 1],
+                strides=[1, 1, 1, 1],
+                rates=[1, 1, 1, 1],
+                padding="SAME",
+            )
+            # Remove the extra dimensions
+            input = tf.squeeze(input_one, axis=0)
+        else:
+            # Add an empty dimension (usually used by the extract window depth)
+            input = tf.expand_dims(input, axis=-1)
 
-        # Reshape the "type" information and combine it with each node in the
-        # sequence so the nodes have context for the current task
-        #
-        # [Batch, len(Type)] => [Batch, 1, len(Type)]
-        type_with_batch = type[:, tf.newaxis, :]
-        # Repeat the type values for each node in the sequence
-        #
-        # [Batch, 1, len(Type)] => [Batch, len(Sequence), len(Type)]
-        type_tiled = tf.tile(type_with_batch, [1, sequence_length, 1])
-        # Concatenate them yielding nodes with length [seq + extract_len + type_len]
-        query = tf.concat([type_tiled, input], axis=2)
+        if self.encode_tokens_with_type:
+            # Reshape the "type" information and combine it with each node in the
+            # sequence so the nodes have context for the current task
+            #
+            # [Batch, len(Type)] => [Batch, 1, len(Type)]
+            type_with_batch = type[:, tf.newaxis, :]
+            # Repeat the type values for each node in the sequence
+            #
+            # [Batch, 1, len(Type)] => [Batch, len(Sequence), len(Type)]
+            type_tiled = tf.tile(type_with_batch, [1, sequence_length, 1])
+            # Concatenate them yielding nodes with length [seq + extract_len + type_len]
+            input = tf.concat([type_tiled, input], axis=2)
 
-        # Embed the contextual nodes
-        query = self.token_embedding(query)
+        query = self.token_embedding(input)
         query = tf.reshape(query, [batch_size, sequence_length, -1])
-        query = self.resnet(query)
 
         # Embed the valid move mask for the attention layer
         value = self.mask_embedding(tf.convert_to_tensor(features.mask))
---
+++ mathy/agent/layers/keras_self_attention/README.md
@@ -0,0 +1,5 @@
+## Self Attention
+
+From: https://pypi.org/project/keras-self-attention/
+
+Inlined because tensorflow needs the `tensorflow.keras` import variants
