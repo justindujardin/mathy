5d5056811c24d3ed8526f076bbf72bf4e63ed4a0
Justin DuJardin <justin@dujardinconsulting.com>
2019-10-20 16:38:00 -0700

    :sparkles: add attention stack dropout as in AYNIA paper

--- mathy/agent/layers/attention_stack.py
+++ mathy/agent/layers/attention_stack.py
@@ -6,11 +6,12 @@ from .keras_self_attention.multi_head_attention import MultiHeadAttention
 class MultiHeadAttentionStack(tf.keras.layers.Layer):
     """Stack of Multi-Head attention layers"""
 
-    def __init__(self, num_layers=2, num_heads=4, **kwargs):
+    def __init__(self, num_layers=2, num_heads=4, dropout_rate=0.1, **kwargs):
         super(MultiHeadAttentionStack, self).__init__(**kwargs)
         self.stack_height = num_layers
         self.norm = tf.keras.layers.BatchNormalization()
         self.add = tf.keras.layers.Add()
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
         self.stack = []
         for i in range(self.stack_height):
             self.stack.append(MultiHeadAttention(num_heads, name=f"{self.name}_{i}"))
@@ -24,5 +25,6 @@ class MultiHeadAttentionStack(tf.keras.layers.Layer):
         output = layer(input_tensor)
         for layer in stack:
             attn_out = layer(output)
+            attn_out = self.dropout(attn_out)
             output = self.norm(self.add([attn_out, output]))
         return output
