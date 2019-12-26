edd35f86104d2763d17a153870a0a6dc989333bb
justindujardin <justin@dujardinconsulting.com>
2019-12-26 13:33:25 -0800

    chore: add netlify runtime file

---
+++ libraries/website/tools/netlify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Building Netlify site..."
+mkdocs build
--- tools/netlify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pip install virtualenv
-
-cd libraries/website
-
-sh tools/setup.sh
-
-sh tools/build.sh
