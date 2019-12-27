7aa04c6292053666572b425c1d61831758e7a734
justindujardin <justin@dujardinconsulting.com>
2019-12-27 11:09:45 -0800

    chore: install pytest during ci build

--- libraries/mathy_pydoc_markdown/tools/setup.sh
+++ libraries/mathy_pydoc_markdown/tools/setup.sh
@@ -8,6 +8,7 @@ if [ ! -d "${DIR}" ]; then
   python -m virtualenv .env -p python3.6
 fi
 
+pip install pytest pytest-cov
 . .env/bin/activate
 echo "Installing/updating requirements..."
 pip install -e .[dev]
