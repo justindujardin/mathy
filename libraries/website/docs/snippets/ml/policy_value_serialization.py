f2263e37a0295f1f7d5beceed97e8f693d08125f
justindujardin <justin@dujardinconsulting.com>
2019-12-31 11:24:09 -0800

    test(a3c): add more a3c actions strategy tests

--- libraries/website/docs/snippets/ml/policy_value_serialization.py
+++ libraries/website/docs/snippets/ml/policy_value_serialization.py
@@ -10,19 +10,22 @@ model_folder = tempfile.mkdtemp()
 setup_tf_env()
 
 args = A3CConfig(
-    max_eps=1,
+    max_eps=3,
     verbose=True,
-    topics=["poly-combine"],
+    topics=["poly"],
     model_dir=model_folder,
+    update_gradients_every=4,
     num_workers=1,
+    units=4,
+    embedding_units=4,
+    lstm_units=4,
+    print_training=True,
 )
 instance = A3CAgent(args)
 instance.train()
-
 # Load the model back in
 model_two = get_or_create_policy_model(
     args=args, env_actions=PolySimplify().action_size, is_main=True
 )
-
 # Comment this out to keep your model
 shutil.rmtree(model_folder)
