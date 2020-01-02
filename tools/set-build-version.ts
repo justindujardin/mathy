cbee0fb0fd0601409c8e630fa347602eb9cafd7d
justindujardin <justin@dujardinconsulting.com>
2020-01-02 08:49:15 -0800

    fix(deploy): sync mathy/mathy_alpha_sm versions
     - update model.config.json during python package updates

--- tools/set-build-version.ts
+++ tools/set-build-version.ts
@@ -31,6 +31,20 @@ function getBuildVersion() {
 getBuildVersion()
   .then((version: any) => {
     console.log("--- UPDATING build version in python modules to : " + version)
+
+    const modelFiles = [
+      "../libraries/mathy_alpha_sm/mathy_alpha_sm/model.config.json",
+      "../libraries/mathy_alpha_sm/model.config.json"
+    ]
+
+    modelFiles.forEach((modelName: string) => {
+      const modelPath = path.join(__dirname, modelName)
+      const json = require(modelPath)
+      json.version = version
+      const fs = require("fs")
+      fs.writeFileSync(modelPath, JSON.stringify(json, null, 2), "utf8")
+    })
+
     const filePath = path.join(
       __dirname,
       "../libraries/mathy_python/mathy/about.py"
