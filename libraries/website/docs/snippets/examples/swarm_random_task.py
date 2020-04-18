63eaf67455cef5d8e837b7c43ae64cac38e24b54
justindujardin <justin@dujardinconsulting.com>
2020-04-18 15:22:47 -0700

    docs(fragile): add problem solver and data generation examples

---
+++ libraries/website/docs/snippets/examples/swarm_random_task.py
@@ -0,0 +1,14 @@
+#!pip install gym
+
+import random
+
+import gym
+
+from mathy.agents.fragile import SwarmConfig, swarm_solve
+from mathy.envs.gym import MathyGymEnv
+
+config = SwarmConfig(max_iters=10)
+task = random.choice(["poly", "binomial", "complex"])
+env: MathyGymEnv = gym.make(f"mathy-{task}-easy-v0")
+_, problem = env.mathy.get_initial_state(env.env_problem_args)
+swarm_solve(problem.text, config)
