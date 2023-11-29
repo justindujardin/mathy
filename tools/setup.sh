8035165ab7d706c96070ac5360e229131da1e272
Justin DuJardin <justin@dujardinconsulting.com>
2023-11-29 10:40:01 -0800

    chore: allow passing a specific python to setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
-python3 --version
+# Default Python path
+PYTHON_PATH="python3"
+
+# Check if a custom Python path is provided as the first argument
+if [ -n "$1" ]; then
+  PYTHON_PATH="$1"
+fi
+
+echo "Using Python at: $PYTHON_PATH"
+$PYTHON_PATH --version
 
 # Make the virtualenv only if the folder doesn't exist
 DIR=.env
@@ -9,10 +18,9 @@ if [ ! -d "${DIR}" ]; then
   echo "Installing root virtualenv (.env)"
   pip install virtualenv --upgrade
   # The first syntax is for CI (travis) and the OR is for MacOS catalina
-  python -m virtualenv -p python3 .env || virtualenv -p python3 .env
+  $PYTHON_PATH -m virtualenv -p $PYTHON_PATH .env || virtualenv -p $PYTHON_PATH .env
 fi
 echo "Installing/updating requirements..."
 .env/bin/pip install -e ./libraries/mathy_python
 
 sh tools/ci-setup.sh
-
