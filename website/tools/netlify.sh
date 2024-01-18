b21a8535b6a7a135c43a39c93883edf416c23c82
Justin DuJardin <justindujardin@users.noreply.github.com>
2024-01-17 16:14:16 -0800

    chore: fix website deploy after monorepo flattening (#98)
    * chore: hack hack hack netlify script
    
    * chore: boop

--- website/tools/netlify.sh
+++ website/tools/netlify.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e
 # Make the virtualenv only if the folder doesn't exist
-DIR=.env
-if [ ! -d "${DIR}" ]; then
-  echo "Installing root virtualenv (.env)"
-  pip install virtualenv --upgrade
-  # The first syntax is for CI (travis) and the OR is for MacOS catalina
-  python -m virtualenv -p python3 .env || virtualenv -p python3 .env
-fi
-echo "Installing/updating requirements..."
-.env/bin/pip install -r requirements.txt
-
+pip install "mkdocs-material[imaging]"
 
 echo "Building Netlify site..."
 mkdocs build
